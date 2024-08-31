223f95bfbca8ba426cb82fcf63b4a8467186b082
Electron <amirhoseink149@gmail.com>
2024-08-31 12:58:14 +0330

    Update

--- script/app.js
+++ script/app.js
@@ -2,6 +2,7 @@ let toggle = document.querySelector('.header__toggle')
 let mobileMenu = document.querySelector('.header__mobile-menu-wrapper')
 let mobileMenuExit = document.querySelector('.mobile-btn__login')
 let cover = document.querySelector('.cover')
+let faqTop = document.querySelectorAll('.faq-top')
 
 toggle.addEventListener('click',()=>{
     {
@@ -14,3 +15,15 @@ mobileMenuExit.addEventListener('click',()=>{
     mobileMenu.style.right = "-31.8rem"
     cover.style.height = "0"
 })
+
+cover.addEventListener('click' , ()=>{
+    mobileMenu.style.right = "-31.8rem"
+    cover.style.height = "0"
+})
+
+faqTop.forEach(item => {
+    item.addEventListener('click', ()=>{
+        item.nextElementSibling.classList.toggle('faq-active')
+        item.lastElementChild.classList.toggle('faq__arrow--active')
+    })
+});
