134f4a6214aeaf52f53c5e49122cb1988d8024eb
Electron <amirhoseink149@gmail.com>
2024-09-07 12:07:11 +0330

    Update

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@ let mobileMenu = document.querySelector('.header__mobile-menu-wrapper')
 let mobileMenuExit = document.querySelector('.mobile-btn__login')
 let cover = document.querySelector('.cover')
 let faqTop = document.querySelectorAll('.faq-top')
+let grabber = document.querySelector('.comment__slider')
 
 toggle.addEventListener('click',()=>{
     {
@@ -27,3 +28,22 @@ faqTop.forEach(item => {
         item.lastElementChild.classList.toggle('faq__arrow--active')
     })
 });
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//         document.body.style.backgroundColor = "yellow";
+//     } else {
+//         document.body.style.backgroundColor = "pink";
+//     }
+// }
+//
+// // Create a MediaQueryList object
+// var x = window.matchMedia("(max-width: 700px)")
+//
+// // Call listener function at run time
+// myFunction(x);
+//
+// // Attach listener function on state changes
+// x.addEventListener("change", function() {
+//     myFunction(x);
+// });
