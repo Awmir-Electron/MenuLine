95660118276314317e598d704e87c4399c347c34
Electron <amirhoseink149@gmail.com>
2024-09-10 02:22:42 +0330

    Update

--- script/app.js
+++ script/app.js
@@ -4,22 +4,26 @@ let mobileMenuExit = document.querySelector('.mobile-btn__login')
 let cover = document.querySelector('.cover')
 let faqTop = document.querySelectorAll('.faq-top')
 let grabber = document.querySelector('.comment__slider')
+let visibility = document.querySelector('.hidden')
 
 toggle.addEventListener('click',()=>{
     {
         mobileMenu.style.right = "0";
         cover.style.height = "100%"
+        cover.style.opacity = "100%"
     }
 })
 
 mobileMenuExit.addEventListener('click',()=>{
     mobileMenu.style.right = "-31.8rem"
     cover.style.height = "0"
+    cover.style.opacity = "0"
 })
 
 cover.addEventListener('click' , ()=>{
     mobileMenu.style.right = "-31.8rem"
     cover.style.height = "0"
+    cover.style.opacity = "0"
 })
 
 faqTop.forEach(item => {
@@ -28,22 +32,3 @@ faqTop.forEach(item => {
         item.lastElementChild.classList.toggle('faq__arrow--active')
     })
 });
-
-// function myFunction(x) {
-//     if (x.matches) { // If media query matches
-//         document.body.style.backgroundColor = "yellow";
-//     } else {
-//         document.body.style.backgroundColor = "pink";
-//     }
-// }
-//
-// // Create a MediaQueryList object
-// var x = window.matchMedia("(max-width: 700px)")
-//
-// // Call listener function at run time
-// myFunction(x);
-//
-// // Attach listener function on state changes
-// x.addEventListener("change", function() {
-//     myFunction(x);
-// });
